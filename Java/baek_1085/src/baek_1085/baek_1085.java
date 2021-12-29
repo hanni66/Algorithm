6a703b5cfcbf6a200ba8e1d1161388dadc180967
hanni66 <72500673+hanni66@users.noreply.github.com>
2021-12-29 23:10:12 +0900

    java파일 수정

--- Java/baek_1085/src/baek_1085/baek_1085.java
+++ Java/baek_1085/src/baek_1085/baek_1085.java
@@ -1,9 +1,13 @@
+package baek_1085;
+
 /*
 백준 1085
 제출일: 2021.05.24
 제출자: 202014014 김하은
 */
+
 import java.util.Scanner;
+
 public class baek_1085 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
