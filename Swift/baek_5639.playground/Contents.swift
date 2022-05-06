14aebacbbb901762a29296d440ec64f2e6a20d37
hanni66 <haeunkim0807@naver.com>
2022-05-06 17:33:45 +0900

    baek_5639

--- Swift/baek_5639.playground/Contents.swift
+++ Swift/baek_5639.playground/Contents.swift
@@ -1,3 +1,27 @@
 import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+var node = [Int]()
+
+while let s = readLine(), let n = Int(s) {
+    node.append(n)
+}
+
+func postOrder(start : Int, end : Int) {
+    
+    if start > end { return }
+    
+    var index = end+1
+    for i in start...end {
+        if node[start] < node[i] {
+            index = i
+            break
+        }
+    }
+    
+    postOrder(start: start+1, end: index - 1)
+    postOrder(start: index, end: end)
+    print(node[start])
+}
+
+postOrder(start: 0, end: node.count - 1)
