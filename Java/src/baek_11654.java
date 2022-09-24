d2eda9111e95c5fd5b4183b3e92a66fc2c42f3e9
hanni66 <haeunkim0807@gmail.com>
2022-09-24 23:17:51 +0900

    Feat : Java - baek_11654

---
+++ Java/src/baek_11654.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class baek_11654 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char s = sc.next().charAt(0);
+		System.out.print((int)s);
+		sc.close();
+	}
+
+}
