23f36db744292f4716f2a4356001077b8614c6f1
hanni66 <haeunkim0807@gmail.com>
2023-03-16 14:20:54 +0900

    swift : baek_9655

---
+++ Swift/algorithm/algorithm/baek_9655.swift
@@ -0,0 +1,16 @@
+//
+//  baek_9655.swift
+//  algorithm
+//
+//  Created by 김하은 on 2023/03/16.
+//
+
+import Foundation
+
+func b9655() {
+    if (Int(readLine()!)! % 2 != 0) {
+        print("SK")
+    } else {
+        print("CY")
+    }
+}
--- Swift/algorithm/algorithm/main.swift
+++ Swift/algorithm/algorithm/main.swift
@@ -1,4 +1,4 @@
 // 문제 연습
 import Foundation
 
-b11723()
+b9655()
